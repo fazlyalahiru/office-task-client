2316483e1523350afc71d7db3526b3d97e20d1ac
fazlyalahiru <fazlyalahi.ru@gmail.com>
2023-08-25 14:19:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div className="flex h-screen overflow-x-scroll">
+      <div className="flex w-full">
+        <div className="w-1/4 h-screen bg-red-300 flex-shrink-0">hello1</div>
+
+        <div className="w-1/4 h-screen bg-blue-300 flex-shrink-0">hello2</div>
+
+        <div className="w-1/4 h-screen bg-green-300 flex-shrink-0">hello3</div>
+
+        <div className="w-1/4 h-screen bg-yellow-300 flex-shrink-0">hello4</div>
+
+        <div className="w-1/4 h-screen bg-pink-300 flex-shrink-0">hello5</div>
+
+        <div className="w-1/4 h-screen bg-purple-300 flex-shrink-0">hello6</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
